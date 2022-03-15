d31ceac26d06afdaf8f9d9ae9c863fd744468a3b
Rodrigo Couto <r@rodg.co>
2022-03-15 02:38:32 -0300

    Recover config files

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,27 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 
+import path from 'path';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
+		vite: {
+			resolve: {
+				alias: {
+					$artifacts: path.resolve('./src/artifacts')
+				}
+			},
+			define: {
+				global: JSON.stringify({})
+			}
+		}
 	}
 };
 
