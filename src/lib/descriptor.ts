6abbc76fa8ce26f13e6836edea51d0975ca0701b
Rodrigo Couto <r@rodg.co>
2022-03-16 08:02:46 -0300

    adapted descriptor to nouns sdk

--- src/lib/descriptor.ts
+++ src/lib/descriptor.ts
@@ -1,20 +1,69 @@
 import Contract from '$lib/contract';
+import { getContractAddressesForChainOrThrow } from '@nouns/sdk';
 
-const network = <string>import.meta.env.VITE_NETWORK_CHAINID;
-const contractAddress = <string>import.meta.env.VITE_DESCRIPTOR_ADDRESS;
-import { NounsDescriptorABI } from '@nouns/contracts';
-import type { NounsDescriptor as INounsDescriptor } from '@nouns/contracts/dist/typechain/NounsDescriptor';
+import { NounsDescriptorABI } from '@nouns/sdk';
+import type { NounsDescriptor } from '@nouns/contracts/dist/typechain/NounsDescriptor';
+// import type { NounSeed } from '@nouns/assets/dist/types';
+
+const chainId = <string>import.meta.env.VITE_NETWORK_CHAINID;
+const { nounsDescriptor } = getContractAddressesForChainOrThrow(parseInt(chainId, 16));
 
 import type { BigNumberish, ethers } from 'ethers';
 
-class NounsDescriptor extends Contract<INounsDescriptor, null> {
+interface INounsDescriptorState {
+	accessoryCount: BigNumberish;
+	backgroundCount: BigNumberish;
+	bodyCount: BigNumberish;
+	glassesCount: BigNumberish;
+	headCount: BigNumberish;
+}
+
+class NounsDescriptorContract extends Contract<NounsDescriptor, INounsDescriptorState> {
 	constructor(network: string, address: string, abi: ethers.ContractInterface) {
-		super(network, address, abi, null, { forceChain: true });
+		super(
+			network,
+			address,
+			abi,
+			{
+				accessoryCount: null,
+				backgroundCount: null,
+				bodyCount: null,
+				glassesCount: null,
+				headCount: null
+			},
+			{ forceChain: true }
+		);
+	}
+
+	async load() {
+		await this.getAccessoryCount();
+		await this.getBackgroundCount();
 	}
 
 	async getColorFromPalette(palleteIndex: BigNumberish, colorIndex: BigNumberish): Promise<string> {
 		const color = await this.contract.palettes(palleteIndex, colorIndex);
 		return color;
+	}
+
+	async getAccessoryCount() {
+		const accessoryCount = await this.contract.accessoryCount();
+
+		this.state.update((current) => ({
+			...current,
+			accessoryCount
+		}));
+	}
+
+	async getBackgroundCount() {
+		const backGroundNT = this.contract.backgroundCount();
+
+		const accessoryCount = await this.contract.accessoryCount();
+
+		this.state.update((current) => ({
+			...current,
+			accessoryCount
+		}));
+	}
 	}
 
 	async getHead(id: BigNumberish): Promise<string> {
@@ -28,6 +77,10 @@ class NounsDescriptor extends Contract<INounsDescriptor, null> {
 	}
 }
 
-const nounsDescriptor = new NounsDescriptor(network, contractAddress, NounsDescriptorABI);
+const nounsDescriptorInstance = new NounsDescriptorContract(
+	chainId,
+	nounsDescriptor,
+	NounsDescriptorABI
+);
 
-export default nounsDescriptor;
+export default nounsDescriptorInstance;
