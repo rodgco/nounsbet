8047b018bc1b022e2d980739a734cd7b1c85392e
Rodrigo Couto <r@rodg.co>
2022-03-23 10:36:46 -0300

    implement contact db on supabase

--- src/routes/api/send_message.ts
+++ src/routes/api/send_message.ts
@@ -1,16 +1,17 @@
 import type { RequestHandler } from '@sveltejs/kit';
+import { supabase } from '$lib/supabase';
 
 /** @type {import('./api/add_contact').RequstHandler} */
 export const post: RequestHandler = async ({ request }) => {
-	console.log(request.headers.get('referer'));
-
 	const contact = await request
 		.text()
 		.then((str) => new URLSearchParams(str))
 		.then((urlParams) => urlParams.entries())
 		.then((params) => Object.fromEntries(params));
 
-	console.log(contact);
+	const result = await supabase.from('Contacts').insert({ app: 'nounsbet', ...contact });
+
+	console.log(result);
 
 	return {
 		status: 303,
